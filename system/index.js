8f93a507e0df88ecbc110f5d5bce0d754b4d3671
JanjyTapYT <janjytapyt@sdevs.org>
2023-09-06 20:49:41 +0200

    Update index.js

--- system/index.js
+++ system/index.js
@@ -3,13 +3,21 @@ const fs = require('fs');
 const axios = require('axios');
 
 fonts.forEach(async font => {
-    if (fs.existsSync(`./fonts/${font.family.replace(' ', '-')}.ttf`)) return;
-
     const url = `https://fonts.googleapis.com/css?family=${font.family.replace(' ', '+')}`;
     const response = await axios.get(url);
     const fontFile = response.data.match(/url\((.*?)\)/)[1];
     const fontUrl = fontFile.replace(/'|"/g, '');
     const fontResponse = await axios.get(fontUrl, { responseType: 'arraybuffer' });
     
-    fs.writeFileSync(`./fonts/${font.family.replace(' ', '-')}.ttf`, fontResponse.data);
+    if (fs.existsSync(`./fonts/${font.family.replace(' ', '-')}.ttf`)) {
+        fs.unlinkSync(`./fonts/${font.family.replace(' ', '-')}.ttf`);
+
+        console.log(`Removed ${font.family.replace(' ', '-')}.ttf`)
+        fs.writeFileSync(`./fonts/${font.family.replace(' ', '-')}.ttf`, fontResponse.data);
+        console.log(`Overwrote ${font.family.replace(' ', '-')}.ttf`)
+        return;
+    } else {
+        fs.writeFileSync(`./fonts/${font.family.replace(' ', '-')}.ttf`, fontResponse.data);
+        console.log(`Downloaded ${font.family.replace(' ', '-')}.ttf`)
+    }
 });
